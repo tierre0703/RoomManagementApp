bbd95cdd3553b0b581e79bc294d0d8dd6ba9643d
Tierre Jin <khj20130703@hotmail.com>
2020-12-24 06:48:05 -0800

    Initial Commit

---
+++ RoomManagementApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  RoomManagementApp
+//
+//  Created by Tierre on 12/24/20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
